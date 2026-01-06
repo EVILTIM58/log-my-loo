7ec2bfe12470f7fd49aeb3eeada2878f0e163d44
Tim w6dce <eviltim@gmail.com>
2026-01-06 22:27:00 +0000

    File changes

--- src/components/DropModal.jsx
+++ src/components/DropModal.jsx
@@ -28,9 +28,6 @@ export default function DropModal({ open, onOpenChange, onSuccess }) {
       setPhotoPreview(null);
       setError(null);
       setIsGettingLocation(false);
-      
-      // Try to get location automatically in background
-      getLocation();
     }
   }, [open]);
 
